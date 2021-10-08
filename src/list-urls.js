edb35efe4f7665a05a755bd0e33d1ed780c03c54
Odole Kayode <kayode@mypatricia.co>
2021-10-08 22:47:10 +0100

    List URLS

--- src/list-urls.js
+++ src/list-urls.js
@@ -1,13 +1,45 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function Listurls() {
+    const [data, setData] = useState("");
+  const fetchData = async () => {
+    const api = await fetch("http://localhost:8888/api/list");
+    setData(await api.json());
+  };
+  console.log(data);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        
-          List URL's
-       
+      <table className="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Original URL</th>
+            <th scope="col">Shortened URL</th>
+            <th scope="col">Count</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+            {data.data && data.data.map(row => {
+                return(
+                <tr>
+                    <th scope="row" key={row.id}>{row.id}</th>
+                    <td>{row.original_link}</td>
+                    <td>http://localhost:8888/{row.shortened_link}</td>
+                    <td>{row.counter}</td>
+                </tr>
+                )
+            })}    
+        </tbody>
+      </table>
       </header>
     </div>
   );
